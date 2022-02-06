dcc4565b6206f6fe44a116b2acb309d6097d838b
Edd <etidbury@users.noreply.github.com>
2022-02-06 17:58:37 +0000

    use AsyncTCP instead for ESP32 support

--- src/WebSockets.h
+++ src/WebSockets.h
@@ -141,8 +141,10 @@
 #error "network type ESP8266 ASYNC only possible on the ESP mcu!"
 #endif
 
-#include <ESPAsyncTCP.h>
-#include <ESPAsyncTCPbuffer.h>
+//#include <ESPAsyncTCP.h>
+//#include <ESPAsyncTCPbuffer.h>
+#include <AsyncTCP.h>
+
 #define WEBSOCKETS_NETWORK_CLASS AsyncTCPbuffer
 #define WEBSOCKETS_NETWORK_SERVER_CLASS AsyncServer
 
